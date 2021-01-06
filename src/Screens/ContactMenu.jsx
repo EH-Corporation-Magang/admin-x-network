3bf1470c4bc681891c5b6f3f67fecf28b685d66e
Muhammad Farras Jibran <farasjibran@gmail.com>
2021-01-06 10:53:13 +0700

    update in dashboard

---
+++ src/Screens/ContactMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactMenu = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default ContactMenu
--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -5,19 +5,25 @@ import React, {
     useEffect
 } from 'react';
 import ReactPaginate from 'react-paginate';
+import CountUp from 'react-countup';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 const Dashboard = () => {
     const [contact, setContact] = useState('')
+    const [pageCount, setPageCount] = useState(0)
     const [loading, setLoading] = useState(false)
     const [currentPage, setCurrentPage] = useState(1)
-    const [pageCount, setPageCount] = useState(1)
+    const [jumlahContact, setJumlahContact] = useState(0)
+    const [jumlahJob, setJumlahJob] = useState(0)
+    const MySwal = withReactContent(Swal)
     const URL_API = `http://localhost:8000`
 
-    const handlePageChange = (selectedObject) => {
-        setCurrentPage(selectedObject.selected + 1)
-        console.log(selectedObject.selected + 1)
-        fetchContact()
-    }
+    useEffect(() => {
+        if (currentPage) {
+            fetchContact()
+        }
+    }, [currentPage])
 
     const fetchContact = async () => {
         try {
@@ -25,7 +31,6 @@ const Dashboard = () => {
                 method: 'GET'
             })
             const datacontact = await data.json()
-            console.log(datacontact)
             if (datacontact.success) {
                 setPageCount(datacontact.from)
                 setContact(datacontact.result.data)
@@ -37,159 +42,194 @@ const Dashboard = () => {
         }
     }
 
-    useEffect(() => {
+    const handlePageChange = (selectedObject) => {
+        setCurrentPage(selectedObject.selected + 1)
         fetchContact()
+    }
+
+    const getAmountContact = async () => {
+        const datacontact = await fetch(`${URL_API}/contact/count`, {
+            method: 'GET'
+        })
+        const contact = await datacontact.json()
+        setJumlahContact(contact.result.length)
+    }
+
+    const getAmountJob = async () => {
+        const datajob = await fetch(`${URL_API}/job`, {
+            method: 'GET'
+        })
+        const job = await datajob.json()
+        setJumlahJob(job.result.length)
+    }
+
+    useEffect(() => {
+        getAmountContact().then(() => {
+            getAmountJob()
+        })
     }, [])
 
-    return (
-        <>
-            <div className="page-header">
-                <h3 className="page-title"> Dashboard </h3>
-                <nav aria-label="breadcrumb">
-                    <ol className="breadcrumb">
-                        <li className="breadcrumb-item active" aria-current="page">Dashboard</li>
-                    </ol>
-                </nav>
-            </div>
-            <div className="row">
-                <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-9">
-                                    <div className="d-flex align-items-center align-self-start">
-                                        <h3 className="text-success mb-0">5</h3>
+
+    if (loading) {
+        MySwal.close()
+        return (
+            <>
+                <div className="page-header">
+                    <h3 className="page-title"> Dashboard </h3>
+                    <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item active" aria-current="page">Dashboard</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div className="row">
+                    <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-9">
+                                        <div className="d-flex align-items-center align-self-start">
+                                            <h3 className="text-success mb-0">5</h3>
+                                        </div>
+                                    </div>
+                                    <div className="col-3">
+                                        <div className="icon icon-box-success ">
+                                            <span className="mdi mdi-apps icon-item" />
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col-3">
-                                    <div className="icon icon-box-success ">
-                                        <span className="mdi mdi-apps icon-item" />
-                                    </div>
-                                </div>
+                                <h6 className="text-muted font-weight-normal">Application Product</h6>
                             </div>
-                            <h6 className="text-muted font-weight-normal">Application Product</h6>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-9">
+                                        <div className="d-flex align-items-center align-self-start">
+                                            <h3 className="text-primary mb-0"><CountUp end={jumlahContact} duration={5} /></h3>
+                                        </div>
+                                    </div>
+                                    <div className="col-3">
+                                        <div className="icon icon-box-primary">
+                                            <span className="mdi mdi-wechat icon-item" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <h6 className="text-muted font-weight-normal">Contact Message</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-9">
+                                        <div className="d-flex align-items-center align-self-start">
+                                            <h3 className="text-warning mb-0"><CountUp end={jumlahJob} duration={5} /></h3>
+                                        </div>
+                                    </div>
+                                    <div className="col-3">
+                                        <div className="icon icon-box-warning">
+                                            <span className="mdi mdi-briefcase icon-item" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <h6 className="text-muted font-weight-normal">Available Work</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-9">
+                                        <div className="d-flex align-items-center align-self-start">
+                                            <h3 className="mb-0">$31.53</h3>
+                                            <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                                        </div>
+                                    </div>
+                                    <div className="col-3">
+                                        <div className="icon icon-box-success ">
+                                            <span className="mdi mdi-arrow-top-right icon-item" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <h6 className="text-muted font-weight-normal">Expense current</h6>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-9">
-                                    <div className="d-flex align-items-center align-self-start">
-                                        <h3 className="mb-0">$17.34</h3>
-                                        <p className="text-success ml-2 mb-0 font-weight-medium">+11%</p>
-                                    </div>
+                <div className="row">
+                    <div className="col-md-12 grid-margin stretch-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="d-flex flex-row justify-content-between">
+                                    <h4 className="card-title mb-1">People Who Contact Us</h4>
+                                    <p className="breadcrumb-item mb-1"><a href="/contactmenu">See Detail...</a></p>
                                 </div>
-                                <div className="col-3">
-                                    <div className="icon icon-box-success">
-                                        <span className="mdi mdi-arrow-top-right icon-item" />
-                                    </div>
-                                </div>
-                            </div>
-                            <h6 className="text-muted font-weight-normal">Revenue current</h6>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-9">
-                                    <div className="d-flex align-items-center align-self-start">
-                                        <h3 className="mb-0">$12.34</h3>
-                                        <p className="text-danger ml-2 mb-0 font-weight-medium">-2.4%</p>
-                                    </div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="icon icon-box-danger">
-                                        <span className="mdi mdi-arrow-bottom-left icon-item" />
-                                    </div>
-                                </div>
-                            </div>
-                            <h6 className="text-muted font-weight-normal">Daily Income</h6>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-9">
-                                    <div className="d-flex align-items-center align-self-start">
-                                        <h3 className="mb-0">$31.53</h3>
-                                        <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
-                                    </div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="icon icon-box-success ">
-                                        <span className="mdi mdi-arrow-top-right icon-item" />
-                                    </div>
-                                </div>
-                            </div>
-                            <h6 className="text-muted font-weight-normal">Expense current</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12 grid-margin stretch-card">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="d-flex flex-row justify-content-between">
-                                <h4 className="card-title mb-1">People Who Contact Us</h4>
-                                <p className="breadcrumb-item mb-1"><a href="/contactmenu">See Detail...</a></p>
-                            </div>
-                            <div className="row">
-                                {loading ? (
-                                    contact.map((item) => {
-                                        return (
-                                            <div className="col-12" key={item.id}>
-                                                <div className="preview-list">
-                                                    <div className="preview-item border-bottom">
-                                                        <div className="preview-thumbnail">
-                                                            <div className="preview-icon bg-primary">
-                                                                <i className="mdi mdi-message" />
+                                <div className="row">
+                                    {loading ? (
+                                        contact.map((item) => {
+                                            return (
+                                                <div className="col-12" key={item.id}>
+                                                    <div className="preview-list">
+                                                        <div className="preview-item border-bottom">
+                                                            <div className="preview-thumbnail">
+                                                                <div className="preview-icon bg-primary">
+                                                                    <i className="mdi mdi-message" />
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                        <div className="preview-item-content d-sm-flex flex-grow">
-                                                            <div className="flex-grow">
-                                                                <h6 className="preview-subject">{item.email}</h6>
-                                                                <p className="text-muted mb-0">{item.message}</p>
-                                                            </div>
-                                                            <div className="mr-auto text-sm-right pt-2 pt-sm-0">
-                                                                <p className="text-muted">15 minutes ago</p>
+                                                            <div className="preview-item-content d-sm-flex flex-grow">
+                                                                <div className="flex-grow">
+                                                                    <h6 className="preview-subject">{item.email}</h6>
+                                                                    <p className="text-muted mb-0">{item.message}</p>
+                                                                </div>
+                                                                <div className="mr-auto text-sm-right pt-2 pt-sm-0">
+                                                                    <p className="text-muted">{item.created_at.slice(0, 19).replace('T', ' ')}</p>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        )
-                                    })
-                                ) : (
-                                        <div></div>
-                                    )}
+                                            )
+                                        })
+                                    ) : (
+                                            <div>Loading...</div>
+                                        )}
+                                </div>
                             </div>
+                            {loading ? (
+                                <ReactPaginate
+                                    pageCount={pageCount}
+                                    onPageChange={handlePageChange}
+                                    containerClassName={'paginate'}
+                                    previousLinkClassName={'previous'}
+                                    breakClassName={'break'}
+                                    nextLinkClassName={'next'}
+                                    pageClassName={'page'}
+                                    disabledClassName={'disabled'}
+                                    activeClassName={'activepaginate'}
+                                />
+                            ) : (
+                                    <div></div>
+                                )}
                         </div>
-                        {loading ? (
-                            <ReactPaginate
-                                pageCount={pageCount}
-                                onPageChange={handlePageChange}
-                                containerClassName={'paginate'}
-                                previousLinkClassName={'previous'}
-                                breakClassName={'break'}
-                                nextLinkClassName={'next'}
-                                pageClassName={'page'}
-                                disabledClassName={'disabled'}
-                                activeClassName={'activepaginate'}
-                            />
-                        ) : (
-                                <div></div>
-                            )}
                     </div>
                 </div>
-            </div>
-        </>
+            </>
+        )
+    } else {
+        MySwal.fire({
+            title: 'Loading...',
+            didOpen: () => {
+                MySwal.showLoading()
+            },
+        })
+    }
+    return (
+        <p></p>
     )
 }
 
